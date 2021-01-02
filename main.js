b64ee550548393374ee534b55745c0b6f46768d4
LeoooCode <>
2021-01-02 17:18:03 +0100

    Finished scan logic & help dialog

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 const ctx = document.getElementById("speedCanvas").getContext("2d");
 const playButton = document.getElementById("playButton");
+const resetButton = document.getElementById("resetButton");
 const mainNumber = document.getElementById("mainNumber");
 const mainNumberText = document.getElementById("mainNumberText");
 
 const GAME_TICK = 50;
-const TOTAL_TIME = 10000;
+const TOTAL_TIME = 5000;
 
-const gameState = {
+const DEFAULT_GAME_STATE = {
   running: false,
   lastRealRadians: 0,
   lastRadians: 0,
   distanceTraveled: 0,
   distanceUntilLastTick: 0,
   timePassed: 0,
-  graph: []
+  graph: [],
 };
+
+let gameState = JSON.parse(JSON.stringify(DEFAULT_GAME_STATE));
 
 function drawScene() {
   ctx.clearRect(0, 0, 1000, 1000);
@@ -24,19 +27,38 @@ function drawScene() {
 
   ctx.beginPath();
   ctx.moveTo(100, 500);
-  const max = gameState.graph.reduce((max, curr) => Math.max(max, Math.abs(curr)), 0);
-  gameState.graph.forEach((num, ind) => ctx.lineTo(100 + ind * (800 / (TOTAL_TIME / GAME_TICK)), 500 - 100 / max * num));
+  let max = gameState.graph.reduce(
+    (max, curr) => Math.max(max, Math.abs(curr)),
+    0
+  );
+  if (max == 0) max = 1;
+  gameState.graph.forEach((num, ind) =>
+    ctx.lineTo(
+      100 + ind * (800 / (TOTAL_TIME / GAME_TICK)),
+      500 - (100 / max) * num
+    )
+  );
   ctx.stroke();
 
   ctx.lineWidth = 10;
-  ctx.strokeStyle = "#7280fd";
+  ctx.strokeStyle = "#90CAF9";
 
   ctx.beginPath();
   ctx.arc(500, 500, 400, 0, 2 * Math.PI);
   ctx.stroke();
 
+  ctx.strokeStyle = "#304FFE";
+
+  if (gameState.running) {
+    ctx.beginPath();
+    const timeRad =
+      2 * Math.PI * (gameState.timePassed / TOTAL_TIME) - Math.PI / 2;
+    ctx.beginPath();
+    ctx.arc(500, 500, 400, -Math.PI / 2, timeRad);
+    ctx.stroke();
+  }
+
   ctx.lineWidth = 5;
-  ctx.strokeStyle = "#002cdf";
 
   ctx.beginPath();
   ctx.arc(
@@ -57,13 +79,22 @@ function fail(reason) {
 
 function gameTick() {
   if (gameState.running) {
-    const diff = Math.sign(gameState.distanceTraveled)*(gameState.distanceTraveled - gameState.distanceUntilLastTick);
+    const diff =
+      Math.sign(gameState.distanceTraveled) *
+      (gameState.distanceTraveled - gameState.distanceUntilLastTick);
     console.log(gameState.distanceTraveled, diff);
     gameState.graph.push(diff);
     gameState.distanceUntilLastTick = gameState.distanceTraveled;
     gameState.timePassed += GAME_TICK;
 
-    mainNumberText.innerText = (Math.abs(gameState.distanceTraveled) / Math.PI * 2 / (gameState.timePassed/1000/60)).toFixed(1);
+    mainNumberText.innerText = (
+      ((Math.abs(gameState.distanceTraveled) / Math.PI) * 2) /
+      (gameState.timePassed / 1000 / 60)
+    ).toFixed(1);
+
+    if (gameState.timePassed >= TOTAL_TIME) {
+      endGame();
+    }
   } else {
     gameState.lastRadians = gameState.lastRadians / 1.03;
     if (gameState.lastRadians >= 0.01) gameState.lastRadians -= 0.01;
@@ -74,6 +105,18 @@ function gameTick() {
   setTimeout(gameTick, GAME_TICK);
 }
 
+function endGame(){
+  gameState.running = false;
+  resetButton.style.display = "block";
+}
+
+function reset(){
+  gameState = JSON.parse(JSON.stringify(DEFAULT_GAME_STATE));
+  resetButton.style.display = "none";
+  mainNumber.style.display = "none";
+  playButton.style.display = "block";
+}
+
 gameTick();
 
 function play() {
@@ -82,9 +125,9 @@ function play() {
   mainNumber.classList.add("countdown");
 
   mainNumberText.innerText = "3";
-  setTimeout(() => (mainNumberText.innerText = "2"), 1000-10);
-  setTimeout(() => (mainNumberText.innerText = "1"), 2000-10);
-  setTimeout(() => (mainNumberText.innerText = "GO"), 3000-10);
+  setTimeout(() => (mainNumberText.innerText = "2"), 1000 - 10);
+  setTimeout(() => (mainNumberText.innerText = "1"), 2000 - 10);
+  setTimeout(() => (mainNumberText.innerText = "GO"), 3000 - 10);
   setTimeout(() => {
     mainNumber.classList.remove("countdown");
     gameState.running = true;
@@ -163,6 +206,20 @@ if (navigator.permissions) {
 
 // MDC
 
-const buttonRipple = new mdc.ripple.MDCRipple(
+new mdc.ripple.MDCRipple(
   document.querySelector(".mdc-button")
 );
+new mdc.ripple.MDCRipple(
+  document.querySelector(".mdc-fab")
+);
+const helpDialog = new mdc.dialog.MDCDialog(
+  document.querySelector("#helpDialog")
+);
+
+// Making dialogs accessible
+dialog.listen('MDCDialog:opened', function() {
+  contentElement.setAttribute('aria-hidden', 'true');
+});
+dialog.listen('MDCDialog:closing', function() {
+  contentElement.removeAttribute('aria-hidden');
+});
